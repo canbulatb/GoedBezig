182f9a5b723e4b5c4b6143351d372e5667701c80
canbulatb <126672142+canbulatb@users.noreply.github.com>
2023-12-11 21:17:51 +0100

    .
    .

--- gbSound.py
+++ gbSound.py
@@ -7,13 +7,15 @@ def soundMakeOne(savePath,language,word):
     Sound = gTTS(text=word, lang=language.lower(), slow=False)
     Sound.save(savePath+language+".mp3")
 
-def soundMakeOneAll(sheet,pathGbFiles,lang,i):
+def soundMakeOneAll(sheet,pathGbFiles,taal,i,):
     dirPath = (os.path.dirname(sys.argv[0]))
     savePath=dirPath+"/files/"+pathGbFiles+"/"
-    soundMakeOne(savePath+str(i),lang[0],sheet[i][2])
-    soundMakeOne(savePath+str(i),lang[1],sheet[i][3])
-    soundMakeOne(savePath+str(i),"Nl",sheet[i][1])
-
+    try:
+        soundMakeOne(savePath+str(i),taal,sheet[i][2])
+        return True
+    except:
+        return False
+    
         
 def soundMake(sheet,pathGbFiles,lang):  
     dirPath = (os.path.dirname(sys.argv[0]))
